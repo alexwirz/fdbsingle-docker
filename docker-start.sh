de884280b61ab352d2c7ee45937f87a708c75a11
Alexander Wirz <alex.wirz@gmail.com>
2018-04-22 14:08:24 +0200

    provide id for the db node and fdb.cluster file with the same id for the client

--- docker-start.sh
+++ docker-start.sh
@@ -6,7 +6,7 @@ fi
 
 if [ ! -f /etc/foundationdb/fdb.cluster ]; then
     ADDR=127.0.0.1
-    echo "docker:$HOSTNAME@$ADDR:4500" >/etc/foundationdb/fdb.cluster
+    echo "docker:d3c9f2ccfca44ee7b03caf2e6dbe4d25@$ADDR:4500" >/etc/foundationdb/fdb.cluster
     chmod 0644 /etc/foundationdb/fdb.cluster
     NEWDB=yes
 fi
